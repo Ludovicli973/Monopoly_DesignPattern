a205dbf0382c12d2f5645df65e9f159a620556b5
Ludovicli973 <ludovicli973@gmail.com>
2022-01-02 15:09:35 +0100

    Update

--- Monopoly_Alexandre_GROSSE_Ludovic_LI/Monopoly_Alexandre_GROSSE_Ludovic_LI/Game_board.cs
+++ Monopoly_Alexandre_GROSSE_Ludovic_LI/Monopoly_Alexandre_GROSSE_Ludovic_LI/Game_board.cs
@@ -11,18 +11,24 @@ namespace Monopoly_Alexandre_GROSSE_Ludovic_LI
         List<Player> players;
         Dice _dice;
         static Game_board _instance;
+        bool end;
+        int turnNumber;
+        PlayerFacade facade;
 
-        private Game_board(List<Player>players) 
+        private Game_board()
         {
-            this.players = players;
+            this.players = new List<Player>();
             this._dice = new Dice();
+            this.end = false;
+            this.turnNumber = 1;
+            this.facade = new PlayerFacade();
         }
 
-        public static Game_board GetInstance(List<Player> players)
+        public static Game_board GetInstance()
         {
-            if(_instance==null)
+            if (_instance == null)
             {
-                _instance = new Game_board(players);
+                _instance = new Game_board();
             }
             return _instance;
         }
@@ -32,9 +38,90 @@ namespace Monopoly_Alexandre_GROSSE_Ludovic_LI
             string msg = "Players :\n";
             foreach (Player p in players)
             {
-                msg += p+"\n";
+                msg += p + "\n";
             }
             return msg;
+        }
+
+
+        // Adding a new player
+        // return true when the player is added, false when he isn't
+        public bool AddPlayer(string namePlayer)
+        {
+            Player newPlayer = new Player(namePlayer);
+
+            if (!players.Contains(newPlayer))
+            {
+                players.Add(newPlayer);
+                return true;
+            }
+            return false;
+        }
+
+
+        // Initializing the players
+        public void StartGame()
+        {
+            Console.WriteLine("Hello and welcome to the Monopoly 2.0 !\n");
+            Console.WriteLine("How many players are you ?");
+
+            bool correct = int.TryParse(Console.ReadLine(), out int nbPlayers);
+
+            while (!correct || nbPlayers < 2)
+            {
+                Console.WriteLine("Incorrect, try again :");
+                correct = int.TryParse(Console.ReadLine(), out nbPlayers);
+            }
+
+            for (int i = 1; i <= nbPlayers; i++)
+            {
+                bool playerAdded = false;
+
+                while (!playerAdded)
+                {
+                    Console.WriteLine("Enter a name for player " + i + ":");
+                    playerAdded = AddPlayer(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Let's start the game !");
+        }
+
+
+        public void PlayingGame()
+        {
+            while (!end)
+            {
+                Console.WriteLine("\n\n\n######################## TURN N°" + turnNumber + " ########################");
+
+                bool playAgain;
+
+                foreach (Player p in players)
+                {
+                    Console.WriteLine(p.Name + ", you're at position " + p.Position);
+                    Console.WriteLine("It's your turn, press any key to roll the dice :");
+                    Console.ReadKey();
+
+                    do
+                    {
+                        _dice.RollDice();
+                        Console.WriteLine(_dice);
+
+                        playAgain = facade.Move(p, _dice);
+                        Console.WriteLine(p + "\n");
+
+                        if (playAgain)
+                        {
+                            Console.WriteLine("You made a double ! Press any key to roll the dice again :");
+                            Console.ReadKey();
+                        }
+
+                    } while (playAgain);
+
+                }
+
+                turnNumber++;
+            }
         }
     }
 }
